79bc74bc7b47685073ba74e056f1d602a8e9e754
Tripoli80 <tripoli80@gmail.com>
2022-09-21 08:46:32 +0200

    archi chenge

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,74 @@
-import { Section } from "components/Section/Section";
-import { Statistics } from "components/Section/Statistics/Statistics";
+import { getKeyArrayFrimOdject } from 'helper/getKeyArrayFrimOdject';
+import useMyBtnFeedback from 'hooks/useMyBtnFeedback';
 
-import React, { Component } from "react";
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+import { Section } from 'components/Section/Section';
+import { Statistics } from 'components/Section/Statistics/Statistics';
+
+const App = () => {
+  const [good, setGood] = useMyBtnFeedback(0);
+  const [neutral, setNeutral] = useMyBtnFeedback(0);
+  const [soso, setSoso] = useMyBtnFeedback(0);
+  const [bad, setBad] = useMyBtnFeedback(0);
+
+  const state = {
+    good,
+    bad,
+    neutral,
+    soso,
   };
 
-  feedBackAdd = (key) => {
-    this.setState((prevState) => {
-      return { [key]: prevState[key] + 1 };
-    });
+  const feedBackAdd = key => {
+    switch (key) {
+      case 'good':
+        setGood();
+        break;
+      case 'bad':
+        setBad();
+        break;
+      case 'neutral':
+        setNeutral();
+        break;
+      case 'soso':
+        setSoso();
+        break;
+      default:
+        console.log(`Не знайшов методу до цієї кнопки - ${key} `);
+    }
   };
-  render() {
-    return (
-      <div>
-        <Section
-          state={this.state}
-          feedBackAdd={this.feedBackAdd}
-          sectionTitel="Leave yuor feedback"
-        />
-        <Statistics state={this.state} />
-      </div>
-    );
-  }
-}
+
+  const countTotalFeedback = () => {
+    let summs = 0;
+    for (const key in state) {
+      if (Object.hasOwnProperty.call(state, key)) {
+        summs += state[key];
+      }
+    }
+    return summs;
+  };
+  const countFeedbackPercentageByKey = key => {
+    return Math.round((state[key] * 100) / countTotalFeedback(state))
+      ? Math.round((state[key] * 100) / countTotalFeedback(state))
+      : '0';
+  };
+
+  const total = countTotalFeedback();
+  const goodPercent = countFeedbackPercentageByKey('good');
+  const btnNames = getKeyArrayFrimOdject(state);
+  return (
+    <div>
+      <Section
+        state={state}
+        feedBackAdd={feedBackAdd}
+        sectionTitel="Leave yuor feedback"
+      />
+      <Statistics
+        state={state}
+        total={total}
+        goodPercent={goodPercent}
+        btnNames={btnNames}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Section/Statistics/Statistics.jsx
+++ src/components/Section/Statistics/Statistics.jsx
@@ -4,43 +4,25 @@ import {
   Element,
   Quntity,
   NoClick,
-} from "./Statistics.styled";
-import { getKeyArrayFrimOdject } from "components/App/Helper/getKeyArrayFrimOdject";
-export const Statistics = ({ state }) => {
-  const btnNames = getKeyArrayFrimOdject(state);
-
-  function countTotalFeedback(state) {
-    let summs = 0;
-
-    for (const key in state) {
-      if (Object.hasOwnProperty.call(state, key)) {
-        summs += state[key];
-      }
-    }
-    return summs;
-  }
-  function countFeedbackPercentageByKey(state, key) {
-    return Math.round((state[key] * 100) / countTotalFeedback(state))
-      ? Math.round((state[key] * 100) / countTotalFeedback(state))
-      : "0";
-  }
+} from './Statistics.styled';
+export const Statistics = ({ state, total, goodPercent, btnNames }) => {
+  const allFeadbackBtns = btnNames.map(btn => (
+    <Element key={btn}>
+      {btn}: <Quntity> {state[btn]}</Quntity>
+    </Element>
+  ));
 
   return (
     <Container>
       <Titel>Statistics</Titel>
-      {countTotalFeedback(state) ? (
+      {total ? (
         <>
-          {btnNames.map((btn) => (
-            <Element key={btn}>
-              {btn}: <Quntity> {state[btn]}</Quntity>
-            </Element>
-          ))}
+          {allFeadbackBtns}
           <Element>
-            Total: <Quntity>{countTotalFeedback(state)}</Quntity>
+            Total: <Quntity>{total}</Quntity>
           </Element>
           <Element>
-            Positive feedback:{" "}
-            <Quntity>{countFeedbackPercentageByKey(state, "good")} %</Quntity>
+            Positive feedback: <Quntity>{goodPercent} %</Quntity>
           </Element>
         </>
       ) : (
